3edcb70a8c51b81fdb0be94367dfadd768dbde8a
Thiago Ferreira <thiagoferreira435@gmail.com>
2020-09-22 21:05:46 -0300

    sprint 4 - teste

--- config/databaseConnection.js
+++ config/databaseConnection.js
@@ -3,9 +3,9 @@ var mysql = require('mysql')
 var connectMySQL = function() {
     return mysql.createConnection({
         host: 'localhost',
-        user: 'mapetliv_default',
-        password: '6*aN2t8w',
-        database: 'mapetliv_default'
+        user: 'root',
+        password: '',
+        database: 'mapet'
     })
 }
 
